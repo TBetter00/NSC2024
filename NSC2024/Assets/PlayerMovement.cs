a4244fca693541517767e3388919be8c85d93460
TBetter00 <125780472+TBetter00@users.noreply.github.com>
2024-05-30 00:42:23 +0700

    add 2 player movement system

--- NSC2024/Assets/PlayerMovement.cs
+++ NSC2024/Assets/PlayerMovement.cs
@@ -4,29 +4,89 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header ("Input Type")]
+    public bool isMain = true;
+
     [Header ("Movement Variable")]
     public float Movespeed = 5f;
     [HideInInspector] public Rigidbody2D rb;
     Vector2 movement;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
+
     void FixedUpdate()
     {
         //Movement here
         rb.MovePosition(rb.position + movement * Movespeed * Time.fixedDeltaTime);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if(isMain == true)
+        {
+            GetInputWASD();
+        }
+        else
+        {
+            GetInputAlt();
+        }
+    }
+
+    // Update is called once per frame
+    void GetInputWASD()
+    {
+        movement = Vector2.zero; // Reset movement
+
         //Input here
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-        if(movement != Vector2.zero){
+        if(Input.GetKey(KeyCode.W))
+        {
+            movement.y = 1f;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            movement.y = -1f;
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            movement.x = -1f;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            movement.x = 1f;
+        }
+        if(movement != Vector2.zero)
+        {
             movement.Normalize();
         }
     }
-    
+
+    void GetInputAlt()
+    {
+        movement = Vector2.zero; // Reset movement
+
+        //Input here
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            movement.y = 1f;
+        }
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            movement.y = -1f;
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            movement.x = -1f;
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            movement.x = 1f;
+        }
+        if(movement != Vector2.zero)
+        {
+            movement.Normalize();
+        }
+    }
 }
